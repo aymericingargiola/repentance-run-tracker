bfca452997f1a659d6adfd7cbb89cfa687034f6c
Aymeric Ingargiola <aymeric.ingargiola@versusmind.eu>
2021-11-23 11:05:57 +0100

    clean file system tools and change mod version

--- src/background/tools/fileSystem.js
+++ src/background/tools/fileSystem.js
@@ -1,17 +1,13 @@
-const util = require("util");
-const fs = require('fs').promises;
-const fsSync = require('fs');
-const readFile = util.promisify(fs.readFile);
-const writeFile = util.promisify(fs.writeFile);
-const path = require('path');
+const fs = require('fs');
+const fsPromises = fs.promises
 module.exports = {
   dirExist: function(dir) {
-    if (!fsSync.existsSync(dir)) return false
+    if (!fs.existsSync(dir)) return false
     else return true
   },
   dirResolve: function(dir) {
-    if (!fsSync.existsSync(dir)) {
-      fsSync.mkdirSync(dir, {
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, {
         recursive: true
       });
     }
@@ -21,7 +17,7 @@ module.exports = {
     if (logs) console.time(`Write "${dir}\\${fileName}" in `)
     module.exports.dirResolve(dir)
     try {
-      await fs.writeFile(`${dir}\\${fileName}`, datas, function(error) {if(error) console.log(error)});
+      await fsPromises.writeFile(`${dir}\\${fileName}`, datas);
     } catch (error) {
       console.log(error)
       return false
@@ -35,12 +31,14 @@ module.exports = {
   },
   readFileAsync: async function(dir, fileName, logs = true) {
     if (logs) console.time(`Read "${dir}\\${fileName}" in `)
-    module.exports.dirExist(dir)
-    try {
-      file = await readFile(`${dir}\\${fileName}`, function(error) {if(error) console.log(error)})
-    } catch (error) {
-      console.log(error)
-      return false
+    let file
+    if (module.exports.dirExist(dir)) {
+      try {
+        file = await fsPromises.readFile(`${dir}\\${fileName}`)
+      } catch (error) {
+        console.log(error)
+        return false
+      }
     }
     if (logs) console.timeEnd(`Read "${dir}\\${fileName}" in `)
     return file;
