174a7bbb9cf581942f4d632b18e598ff784fbd31
Aymeric Ingargiola <aymericfbm@gmail.com>
2022-06-28 22:56:31 +0200

    remove devtools on build version

--- src/background/main.js
+++ src/background/main.js
@@ -178,7 +178,6 @@ async function createWindow() {
     createProtocol('app')
     // Load the index.html when not in development
     win.loadURL('app://./index.html')
-    win.webContents.openDevTools()
   }
 
   //Check for app updates
